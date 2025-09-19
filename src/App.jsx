b3deea171c7bc0265dfe51b89e0951ee01c52c98
Samarinara <samkatevatis@gmail.com>
2025-09-18 22:48:42 -0600

    randomised numbers on homepage

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,26 @@ import Hand from './Hand.jsx'
 import { PlayingCard, ClubIcon, HeartIcon, DiamondIcon, SpadeIcon } from './PlayingCard.jsx';
 import { CoolTitle } from './CoolTitle.jsx';
 
-
 function App() {
+  const card1 = Math.floor(Math.random() * 9 + 2).toString();
+  const card2 = Math.floor(Math.random() * 9 + 2).toString();
+  const card3 = Math.floor(Math.random() * 9 + 2).toString();
+  const card4 = Math.floor(Math.random() * 9 + 2).toString();
+  const card5 = Math.floor(Math.random() * 9 + 2).toString();
+
   return (
     <div className='App'>
       <Hand>
-        <PlayingCard Suit={SpadeIcon} number='Q'>hello</PlayingCard>
-        <PlayingCard Suit={HeartIcon} color="red" number='J'>hello</PlayingCard>
-        <PlayingCard Suit={ClubIcon} number='2'>Welcome to Sam Katevatis' portfolio! Click on cards for more info </PlayingCard>
-        <PlayingCard Suit={DiamondIcon} color="red"number='7'>hello</PlayingCard>
-        <PlayingCard Suit={HeartIcon}  color="red" number='K'>hello</PlayingCard>
+        <PlayingCard Suit={SpadeIcon} number={card1}>hello</PlayingCard>
+        <PlayingCard Suit={HeartIcon} color="red" number={card2}>hello</PlayingCard>
+        <PlayingCard Suit={DiamondIcon} color="red" number={card3}>
+          <div className='card-text'>
+            <h4>Hi I'm Sam</h4>
+            I love making things that work
+          </div>
+        </PlayingCard>
+        <PlayingCard Suit={ClubIcon} color="black" number={card4}>hello</PlayingCard>
+        <PlayingCard Suit={HeartIcon}  color="red" number={card5}>hello</PlayingCard>
 
       </Hand>
 
